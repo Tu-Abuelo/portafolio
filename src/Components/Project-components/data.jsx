c17e68559bc1f0a197fd50ebbb2e8a10d93b0089
Tu-Abuelo <luisalbertovaldes1423@gmail.com>
2024-10-08 09:05:11 -0400

    Portafolio1.3

--- src/Components/Project-components/data.jsx
+++ src/Components/Project-components/data.jsx
@@ -5,13 +5,13 @@ import { BiLogoJavascript } from 'react-icons/bi'
 
 const projects = [
   {
-    src: 'Sin título.png',
+    src: '../../../public/Sin título.png',
     name: 'Giphy Clone',
     href: 'https://tu-abuelo.github.io/buscador-de-gifs/',
     tecn: [<FaHtml5 key='html' className='icons html' />, <FaCss3Alt key='css' className='icons css' />, <BiLogoJavascript key='javascript' className='icons javascript' />, <FaReact key='react' className='icons react' />]
   },
   {
-    src: 'Sin título.png',
+    src: '../../../public/Sin título.png',
     name: 'Giphy Clones',
     href: 'https://tu-abuelo.github.io/buscador-de-gifs/',
     tecn: [<FaHtml5 key='html' className='icons html' />, <FaCss3Alt key='css' className='icons css' />, <BiLogoJavascript key='javascript' className='icons javascript' />, <FaReact key='react' className='icons react' />]
